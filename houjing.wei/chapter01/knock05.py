3471565b4e541abe2fc97e4541d76e59f4eb052c
Houjing.Wei <38242930+Vita112@users.noreply.github.com>
2018-10-25 10:28:51 +0800

    Update knock05.py

--- houjing.wei/chapter01/knock05.py
+++ houjing.wei/chapter01/knock05.py
@@ -2,11 +2,19 @@
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，
 # "I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
 
-raw_t = 'I am an NLPer'
-w_list = raw_t.strip().split()
-c_list = list(raw_t)
-w_list2 = t_list[1:]
-c_list2 = c_list[1:]
-bigramw = zip(w_list, w_list2)
-bigramc = zip(c_list, c_list2)
-print bigramw, bigramc
+# 定义一个n-gram函数
+def n_gram(wlist, n):
+    n_wlist = []
+    for i in range(n):
+        n_wlist.append(wlist[i:])
+    return zip(*n_wlist)
+      
+  
+if __name__ == '__main__':  
+    raw_t = 'I am an NLPer'
+    w_list = raw_t.strip().split()
+    c_list = list(raw_t)
+
+    print n_gram(w_list, 2)
+    print n_gram(c_list, 2)
+
