2de35bf29fd654f6acfb408a43461932c7400b1d
houjing.wei <weihoujing@163.com>
2018-10-30 18:44:22 +0800

    commit knock10

--- houjing.wei/chapter02/knock10.py
+++ houjing.wei/chapter02/knock10.py
@@ -1,2 +1,13 @@
 # 10. 行数のカウント
 # 行数をカウントせよ．確認にはwcコマンドを用いよ．
+
+def count_line(path):
+    count = 0
+    for index, line in enumerate(open(r'path', 'r')):
+       count += 1
+
+   # return count
+
+file = 'H:\weihoujing\PyCharm Community Edition 2018.2.4\100knock2018\hightem.txt'
+print(count_line(file))
+
