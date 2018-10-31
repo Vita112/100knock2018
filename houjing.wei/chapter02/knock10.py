a35ea22c8b03aa0f749c94f640c59e248d587cc1
houjing.wei <weihoujing@163.com>
2018-10-31 15:45:55 +0800

    change knock10

--- houjing.wei/chapter02/knock10.py
+++ houjing.wei/chapter02/knock10.py
@@ -1,13 +1,18 @@
 # 10. 行数のカウント
 # 行数をカウントせよ．確認にはwcコマンドを用いよ．
 
+import os
+
+
 def count_line(path):
-    count = 0
-    for index, line in enumerate(open(r'path', 'r')):
-       count += 1
+    line_num = 0            # 全局变量，函数返回值
+    for i, line in enumerate(open(path,'r')):    # 索引枚举迭代器中的元素，返回迭代器对象
+        line_num += 1
+        # print(line_num,line)   打印行号，内容
+    return line_num
 
-   # return count
-
-file = 'H:\weihoujing\PyCharm Community Edition 2018.2.4\100knock2018\hightem.txt'
-print(count_line(file))
+if __name__ == '__main__':
+    f = '../hightemp.txt'      # 文件相对路径
+    # print(os.getcwd())   打印文件所在路径
+    print(count_line(f))
 
