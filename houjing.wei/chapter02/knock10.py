f6c132711559d1323d5843e0b3fe781bde25f0ec
houjing.wei <weihoujing@163.com>
2018-11-01 18:26:46 +0800

    push txt used for test

--- houjing.wei/chapter02/knock10.py
+++ houjing.wei/chapter02/knock10.py
@@ -2,13 +2,13 @@
 # 行数をカウントせよ．確認にはwcコマンドを用いよ．
 
 import os
-
+import sys
 
 def count_line(path):
     line_num = 0            # 全局变量，函数返回值
-    for i, line in enumerate(open(path,'r')):    # 索引枚举迭代器中的元素，返回迭代器对象
+    for i, line in enumerate(open(path,'r',encoding='utf-8')):     # 索引枚举迭代器中的元素，返回迭代器对象
         line_num += 1
-        # print(line_num,line)   打印行号，内容
+        # print(line_num,line)   # 打印行号，内容
     return line_num
 
 if __name__ == '__main__':
