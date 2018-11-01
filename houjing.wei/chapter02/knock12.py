f5a15537a8c3b36320c7c83797af8867107ccf6f
houjing.wei <weihoujing@163.com>
2018-11-02 07:36:41 +0800

    commit

--- houjing.wei/chapter02/knock12.py
+++ houjing.wei/chapter02/knock12.py
@@ -3,14 +3,30 @@
 # 確認にはcutコマンドを用いよ．
 
 
-def col_list(pathfile, n):
-    result = []
-    for line in open(pathfile,'r').readlines():
-        line_array = line.split(' ')
-        result.append(line_array[n-1] + '\n')
-    print(result)
-
+def col_list(pathfile):
+    # result = []
+    # line_array =[]
+    for line in open(pathfile, 'r', encoding='utf-8'):
+        # line_array = line.split(' ')
+        return line.split(' ')
+        # result.append(line_array[n-1] + '\n')
+    # return line_array
 if __name__ == '__main__':
-    path = '../hightemp.txt'
-    col_list(path,1)
+    path = '../hightemp_test.txt'
+    # col_list(path, 1)
+
+    with open('../col1.txt', 'w',encoding='utf-8') as col1_out, open('../col2.txt', 'w') as col2_out:
+        for col_list in col_list(path):
+            # new_str = ''.join(col_list(path, 1))
+            # print(new_str)
+            col1_out.write(col_list[0])
+            # col2_out.write(new_str)
+
+
+
+
+
+
+
+
 
