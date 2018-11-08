550cd6290590cdd95d69ee4221c495418681a46d
houjing.wei <weihoujing@163.com>
2018-11-08 18:53:46 +0800

    1108

--- houjing.wei/chapter03/knock24.py
+++ houjing.wei/chapter03/knock24.py
@@ -6,17 +6,19 @@ import re
 if __name__ == '__main__':
     with open('../data/knock20.txt', 'r', encoding='utf-8') as f:
         head = re.compile(r'\|略名.+')
-        urls = re.compile(r'\<ref\>.+\</ref\>')
+        mediafile = re.compile(r'.*(ファイル|File)+:(.*[a-zA-Z0-9]+.*)')
         section = re.compile(r'\=\=(.+)\=\=')
         lable = 1
         for line in f.readlines():
             if head.match(line):
                 print(line)
-            if urls.findall(line):
-                    print(line)
+            file = mediafile.findall(line)
+            if file:
+                print(''.join(str(file[0])),''.join(str(file[1])))
             if section.match(line):
                 print('[' + str(lable) + ']:' + line.strip())
                 lable += 1
-            if urls.findall(line):
-                    print(line)
+            # file = mediafile.findall(line)
+            # if file:
+            #     print(file)
 
