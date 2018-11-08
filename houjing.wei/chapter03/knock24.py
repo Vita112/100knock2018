a24ddc2ced4ef40654becbbb55575c63e6395ece
houjing.wei <weihoujing@163.com>
2018-11-08 19:53:03 +0800

    1107

--- houjing.wei/chapter03/knock24.py
+++ houjing.wei/chapter03/knock24.py
@@ -5,20 +5,18 @@ import re
 
 if __name__ == '__main__':
     with open('../data/knock20.txt', 'r', encoding='utf-8') as f:
+        # 把正则表达式编码为pattern对象
         head = re.compile(r'\|略名.+')
-        mediafile = re.compile(r'.*(ファイル|File)+:(.*[a-zA-Z0-9]+.*)')
+        mediafile = re.compile(r'.*(ファイル|File|file):(.*[a-zA-Z0-9]+.*)')
         section = re.compile(r'\=\=(.+)\=\=')
         lable = 1
         for line in f.readlines():
             if head.match(line):
                 print(line)
-            file = mediafile.findall(line)
-            if file:
-                print(''.join(str(file[0])),''.join(str(file[1])))
             if section.match(line):
                 print('[' + str(lable) + ']:' + line.strip())
                 lable += 1
-            # file = mediafile.findall(line)
-            # if file:
-            #     print(file)
+            file = mediafile.findall(line)
+            if file:
+                print(''.join(file[0][0])+':'+''.join(file[0][1]))
 
