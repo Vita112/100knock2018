d7b758ebb7ac86e8f60b53f46c37dd3eac29ef3e
houjing.wei <weihoujing@163.com>
2018-11-19 14:11:30 +0800

    1119

--- houjing.wei/chapter03/knock24.py
+++ houjing.wei/chapter03/knock24.py
@@ -10,13 +10,17 @@ if __name__ == '__main__':
         mediafile = re.compile(r'.*(ファイル|File|file):(.*[a-zA-Z0-9]+.*)')
         section = re.compile(r'\=\=(.+)\=\=')
         lable = 1
-        for line in f.readlines():
+        for line in f.readlines():    # 遍历逐行读取文件所有行，line为字符串
             if head.match(line):
                 print(line)
-            if section.match(line):
+            if section.match(line):       # 列出并打印分区名
                 print('[' + str(lable) + ']:' + line.strip())
                 lable += 1
             file = mediafile.findall(line)
+            '''findall(str)将在str中从最开始查找符合pattern对象的内容，返回列表。
+            本题列表中只有一个元组元素，元组中包含两个字符串
+            '''
             if file:
-                print(''.join(file[0][0])+':'+''.join(file[0][1]))
+                print(''.join(file[0][0])+':'+''.join(file[0][1]))   # 列表元素字符串化
 
+    # 本题的最终输出效果为：输出每个分区名，列出每个分区下的文件名。
