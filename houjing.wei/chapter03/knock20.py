0113c3ef402332fdb9145acbf0a1027a01b16faf
houjing.wei <weihoujing@163.com>
2018-11-19 18:43:54 +0800

    1119

--- houjing.wei/chapter03/knock20.py
+++ houjing.wei/chapter03/knock20.py
@@ -18,4 +18,12 @@ if __name__ == '__main__':
     path = '../data/jawiki-country.json'
     get_UK_data(path)
 
-
+'''JSON是一种轻量级的数据交换格式。python中导入json库后，
+json.dumps()：将python对象（dic）编码为JSON字符串（str），实现格式化输出。
+              参数sort_keys=True时，按照字典排序对对象排序；
+              参数indent=4表示缩进；
+              参数separator的作用是：去掉“""和：”后面的空格，精简数据，方便传输
+              参数skipkeys：跳过非string对象，不将其作为key处理。
+json.dump():将数据写入json文件中。
+json.loads()：将已编码的JSON字符串（str）解码为Python对象（dic）。
+json.load()：读取json文件，并把字符串解python数据类型。'''
