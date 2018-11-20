c04b25c9373fc210371126127e2a35305cfed348
houjing.wei <weihoujing@163.com>
2018-11-20 20:20:26 +0800

    1120

--- houjing.wei/chapter03/knock25.py
+++ houjing.wei/chapter03/knock25.py
@@ -1,11 +1,11 @@
 '''25. テンプレートの抽出
 記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ．'''
 
-# 打开并解码json文件
+
 def basicinfo(path):
     dic = {}
     with open(path, 'r', encoding='utf-8') as f:
-        fstr = f.read().replace('\n', '  ').replace('\n\n','  ').split('  ')     # class str，以双空格切分字符串后，存入列表
+        fstr = f.read().replace('\n', '  ').replace('\n\n','  ').split('  ')     # class str，读取文件；以双空格切分字符串后，存入列表
         # print(fstr)
         for i in fstr:
             if '基礎情報' in i:
@@ -13,12 +13,12 @@ def basicinfo(path):
             if '=' in i:
                 i = i.split(' = ')
                 dic[i[0]] = i[1]
-                print(dic)
+                # print(dic)
             if i == '}}':     # 当代码为 "'if '}}' in i:  break  "时，为什么不显示"print(dic)"打印结果。
                 break
-
+    return print(dic)
 
 if __name__ == '__main__':
-    pathfile = '../data/knock20.json'
+    pathfile = '../data/knock20.txt'
     basicinfo(pathfile)
 
---
+++ houjing.wei/chapter03/knock26.py
@@ -0,0 +1,20 @@
+'''26. 強調マークアップの除去
+25の処理時に，テンプレートの値からMediaWikiの強調マークアップ
+（弱い強調，強調，強い強調のすべて）を除去してテキストに変換せよ（参考: マークアップ早見表）．'''
+import sys
+sys.path.append(r'H:\weihoujing\PyCharm Community Edition 2018.2.4\projects\houjing.wei\chapter03')
+from knock25 import basicinfo
+import re
+
+dic = basicinfo(path)
+emphasis = re.compile(r'(\'\'+)|"')
+def emphasis_remove(path):
+
+
+
+
+
+
+if __name__ == '__main__':
+    path = '../data/knock20.json'
+    emphasis_remove(path)
