f8ae5753b1936353bc081e1e2919627101da8f1b
houjing.wei <weihoujing@163.com>
2018-11-30 17:52:31 +0800

    1130

--- houjing.wei/chapter03/knock26.py
+++ houjing.wei/chapter03/knock26.py
@@ -7,15 +7,18 @@ from knock25 import basicinfo
 import re
 
 
-def emphasis_remove():
-    dic = basicinfo('../data/knock20.json')
+def emphasis_remove(pathfile):
+    dic = basicinfo(pathfile)
     # print(dic)
     emphasis = re.compile(r'(\'\'+)|"')
     for item in dic:
         dic[item] = emphasis.sub('', dic[item])
+        # re.sub(pattern, repl, string, count=0, flags=0),
+        # 语法解释：在string中按照pattern找到匹配对象，用repl将其替换掉。返回被替换后的string。
+        # repl可以是字符串，或函数。字符串中的\被当做转义符号处理。count表示匹配几次。
     return dic
 
 
-# if __name__ == '__main_':
-    # path = '../data/knock20.json'        # 为什么不返回结果呢？
-print(emphasis_remove())
+if __name__ == '__main__':
+    path = '../data/knock20.json'
+    print(emphasis_remove(path))
--- houjing.wei/chapter03/knock27.py
+++ houjing.wei/chapter03/knock27.py
@@ -1,2 +1,7 @@
 '''27. 内部リンクの除去
 26の処理に加えて，テンプレートの値からMediaWikiの内部リンクマークアップを除去し，テキストに変換せよ（参考: マークアップ早見表）．'''
+from knock26 import emphasis_remove
+import re
+
+dic = emphasis_remove()
+linkMarkup= re.compile()
